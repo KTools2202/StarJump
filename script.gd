0fcf2a83c69b140d7df0dcc47c842c19354066d1
KTools2202 <170725916+KTools2202@users.noreply.github.com>
2024-06-13 21:08:48 +1000

    Update script.gd

--- script.gd
+++ script.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var try = 0
+var try = 1
 var quiz = [1, 2, 3]
 var quizzing = {"question1": "test", "question2": "test2", "question3": "test3"}
 const ANSWER1 = "answer"
@@ -9,12 +9,14 @@ const ANSWER3 = "answer3"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if quizzing == 1:
-		while try >= 1:
+	print("Hello there")
+	if quiz == 1:
+		while try <= 1:
 			attempt1 == input(quizzing["question1"]) # Replace with function body.
 			if attempt1 == ANSWER1:
 				print('Correct!')
@@ -22,15 +24,21 @@ func _process(delta):
 			else:
 				print('Incorrect!')
 	elif quizzing == 2:
-		while try >= 2:
+		while try <= 2:
 			attempt2 == input(quizzing["question2"])
 			if attempt2 == ANSWER2:
 				print('Correct!')
 				try += 1
 			else:
-				print('Incorrect')
+				print('Incorrect!')
 	elif quizzing == 3:
-		
+		while try <= 3:
+			attempt3 == input(quizzing["question3"])
+			if attempt2 == ANSWER2:
+				print('correct')
+				try += 1
+			else:
+				print('Incorrect!')
 	else:
-		pass
+		print("An error has been encountered. Please")
 	pass
