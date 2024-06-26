643b61f673f917b2d3947f4ade53b542e82a545d
KTools2202 <170725916+KTools2202@users.noreply.github.com>
2024-06-26 22:22:17 +1000

    rewrite: literally everything
    I actually know how to do things now

--- script.gd
+++ script.gd
@@ -1,45 +1,39 @@
-extends Node
+extends CharacterBody2D
 
-var try = 1
-var quiz = [1, 2, 3]
-var quizzing = {"question1": "test", "question2": "test2", "question3": "test3"}
-const ANSWER1 = "answer"
-const ANSWER2 = "answer2"
-const ANSWER3 = "answer3"
+var speed = 200
+var sprite_index = 0
+var sprite_textures = []
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-	
+	# Load the textures
+	sprite_textures.append(load("res://sprites/Astronaut Sprite 1.png"))
+	sprite_textures.append(load("res://sprites/Astronaut Sprite 1 fire.png"))
 
+	# Set initial sprite texture
+	$Sprite2D.texture = sprite_textures[sprite_index]
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print("Hello there")
-	if quiz == 1:
-		while try <= 1:
-			attempt1 == input(quizzing["question1"]) # Replace with function body.
-			if attempt1 == ANSWER1:
-				print('Correct!')
-				try += 1
-			else:
-				print('Incorrect!')
-	elif quizzing == 2:
-		while try <= 2:
-			attempt2 == input(quizzing["question2"])
-			if attempt2 == ANSWER2:
-				print('Correct!')
-				try += 1
-			else:
-				print('Incorrect!')
-	elif quizzing == 3:
-		while try <= 3:
-			attempt3 == input(quizzing["question3"])
-			if attempt2 == ANSWER2:
-				print('correct')
-				try += 1
-			else:
-				print('Incorrect!')
-	else:
-		print("An error has been encountered. Please")
-	pass
+	# Handle player movement
+	var velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+
+	move_and_slide()
+
+	# Handle sprite toggle on key press
+	if Input.is_action_just_pressed("toggle_sprite"):
+		toggle_sprite()
+
+func toggle_sprite():
+	# Alternate the sprite texture
+	sprite_index = (sprite_index + 1) % sprite_textures.size()
+	$Sprite2D.texture = sprite_textures[sprite_index]
