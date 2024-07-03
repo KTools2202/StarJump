91b8f5b1230a9b169cc7fe6aad4fa2fc7cfc47f1
KTools2202 <170725916+KTools2202@users.noreply.github.com>
2024-07-03 14:06:34 +1000

    fix: clues not working & player group

--- scripts/clues/Clue1.gd
+++ scripts/clues/Clue1.gd
@@ -1,21 +1,19 @@
 extends Area2D 
 
-@export var pop_up_text: String = "It's the closest star to the Sun!"
+#@export var pop_up_text: String = "It's the closest star to the Sun!"
 
 @onready var label = $Label  # Path may need to be changed if still not functioning
 
 func _ready():
-	connect("body_entered", Callable(self, "_on_body_entered"))
-	connect("body_exited", Callable(self, "_on_body_exited"))
-	label.text = pop_up_text
-	label.hide()
+	label.text = "It's the closest star to the Sun!"
+	label.visible_characters = 0
 
 # Showing text when the player enters the area
 func _on_body_entered(body):
 	if body.is_in_group("player"):  # The player-character is in a group called "player"
-		label.show()
+		label.visible_characters = -1
 
 # Hiding text when the player exits the area
 func _on_body_exited(body):
 	if body.is_in_group("player"):
-		label.hide()
+		label.visible_characters = 0
