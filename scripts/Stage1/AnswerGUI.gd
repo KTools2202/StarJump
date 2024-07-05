0bfc174b3a141fcfabd1d9fd0d3e0bfca9a8c9c5
KTools2202 <170725916+KTools2202@users.noreply.github.com>
2024-07-05 13:28:23 +1000

    fix: answergui logic
    might have to figure out better logic later

--- scripts/Stage1/AnswerGUI.gd
+++ scripts/Stage1/AnswerGUI.gd
@@ -1,18 +1,29 @@
-extends Control
+extends Area2D 
 
-var callback = null
+#@export var pop_up_text: String = "It's the closest star to the Sun!"
+
+@onready var panel = $Panel
+@onready var heading = $Panel/Label
+@onready var body = $Panel/Body
+@onready var Ans1 = $Panel/Ans1
+@onready var Ans2 = $Panel/Ans2
+@onready var Ans3 = $Panel/Ans3
+@onready var Ans4 = $Panel/Ans4
 
 func _ready():
-	$Panel/OK.connect("pressed", Callable(self, "_on_ok_button_pressed"))
-	hide()  # Start hidden
+	heading.text = "QUIZ!"
+	
+	heading.visible_characters = 0
+	body.visible_characters = 0
 
-func show_message(title, body, callback_func = null):
-	$Panel/Title.text = title
-	$Panel/Body.text = body
-	callback = callback_func
-	show()
+# Showing text when the player enters the area
+func _on_body_entered(body):
+	if body.is_in_group("player"):  # The player-character is in a group called "player"
+		heading.visible_characters = -1
+		body.visible_characters = -1
 
-func _on_ans1_button_pressed():
-	hide()
-	if callback and callback is Callable:
-		callback.call()
+# Hiding text when the player exits the area
+func _on_body_exited(body):
+	if body.is_in_group("player"):
+		heading.visible_characters = 0
+		body.visible_characters = 0
