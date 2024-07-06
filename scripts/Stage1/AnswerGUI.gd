b23f2623381066a8d4027ca4d10b3c74d2d43ddc
KTools2202 <170725916+KTools2202@users.noreply.github.com>
2024-07-06 15:43:27 +1000

    fix: body variable names :(

--- scripts/Stage1/AnswerGUI.gd
+++ scripts/Stage1/AnswerGUI.gd
@@ -4,7 +4,7 @@ extends Area2D
 
 @onready var panel = $Panel
 @onready var heading = $Panel/Label
-@onready var body = $Panel/Body
+@onready var middle = $Panel/Body
 @onready var Ans1 = $Panel/Ans1
 @onready var Ans2 = $Panel/Ans2
 @onready var Ans3 = $Panel/Ans3
@@ -12,18 +12,32 @@ extends Area2D
 
 func _ready():
 	heading.text = "QUIZ!"
-	body.text = "Which star are you on?"
+	middle.text = "Which star are you on?"
+	panel.visible = false
 	heading.visible_characters = 0
-	body.visible_characters = 0
+	middle.visible_characters = 0
+	Ans1.visible = false
+	Ans2.visible = false
+	Ans3.visible = false
+	Ans4.visible = false
 
 # Showing text when the player enters the area
 func _on_body_entered(body):
 	if body.is_in_group("player"):  # The player-character is in a group called "player"
+		panel.visible = true
 		heading.visible_characters = -1
-		body.visible_characters = -1
+		middle.visible_characters = -1
+		Ans1.visible = true
+		Ans2.visible = true
+		Ans3.visible = true
+		Ans4.visible = true
 
 # Hiding text when the player exits the area
 func _on_body_exited(body):
 	if body.is_in_group("player"):
 		heading.visible_characters = 0
-		body.visible_characters = 0
+		middle.visible_characters = 0
+		Ans1.visible = false
+		Ans2.visible = false
+		Ans3.visible = false
+		Ans4.visible = false
